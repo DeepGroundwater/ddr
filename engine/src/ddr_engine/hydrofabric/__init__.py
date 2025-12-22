5115ef2e8373ed5ce326a2a90ff38032eae64f91
Tadd Bindas <tkb5476@psu.edu>
2025-12-22 17:40:17 -0600

    engine-tests: added workspace package for engine

--- engine/src/ddr_engine/hydrofabric/__init__.py
+++ engine/src/ddr_engine/hydrofabric/__init__.py
@@ -1,9 +1,15 @@
-"""Functions for building data matrices from the hydrofabric"""
+"""Functions for building data matrices from the v2.2 hydrofabric"""
 
-from .v2_2.adjacency import coo_to_zarr as create_v2_2_conus_coo_to_zarr
-from .v2_2.adjacency import create_matrix as create_v2_2_matrix
+from .v2_2.adjacency import coo_to_zarr, create_matrix, index_matrix
+from .v2_2.gages_adjacency import coo_to_zarr_group, create_coo, find_origin, preprocess_river_network, subset
 
 __all__ = [
-    "create_v2_2_matrix",
-    "create_v2_2_conus_coo_to_zarr",
+    "coo_to_zarr",
+    "create_matrix",
+    "coo_to_zarr_group",
+    "subset",
+    "find_origin",
+    "create_coo",
+    "preprocess_river_network",
+    "index_matrix",
 ]
